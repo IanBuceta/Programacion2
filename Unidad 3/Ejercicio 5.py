51643515ddefab2df494f5778ce9c061bdeb676a
IanBuceta <iansbrizza@gmail.com>
2020-08-28 10:45:15 -0300

    Added comment and size of list

--- Unidad 3/Ejercicio 5.py
+++ Unidad 3/Ejercicio 5.py
@@ -1,4 +1,8 @@
+from sys import getsizeof
+
 numList = [1,2,3,4,5]
 print("Lista + 1:")
+#List comprehension
 mappedList = [num for num in map(lambda numero: numero + 1, numList)]
 print(mappedList)
+print("Size of listComprehension: ", getsizeof(mappedList))
