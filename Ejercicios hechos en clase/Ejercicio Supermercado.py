1cd8c1e9f5b080edcfba8c8689df418204a90be6
IanBuceta <iansbrizza@gmail.com>
2020-09-21 12:04:11 -0300

    Added print statements, removed unecessary libray

--- Ejercicios hechos en clase/Ejercicio Supermercado.py
+++ Ejercicios hechos en clase/Ejercicio Supermercado.py
@@ -1,5 +1,3 @@
-from typing import List
-
 from os import system
 
 
@@ -31,6 +29,7 @@ class supermercado:
         self.__opciones[opcion][1]()
 
     def generar_cajas(self):
+        system("cls")
         cantidad_de_cajas = int(input("Ingrese numero de cajas: "))
         cajas = list()
 
@@ -40,47 +39,68 @@ class supermercado:
         self.__cajas = list(cajas)
 
     def obtener_caja(self):
+        system("cls")
         for caja in self.__cajas:
             print(caja.numero)
 
         return self.__cajas[int(input("Ingrese número de la caja: "))]
 
     def agregar_cliente(self):
+        system("cls")
         caja = self.obtener_caja()
         nombre = input("Ingrese nombre del cliente: ")
         monto = float(input("Ingrese monto del cliente: "))
         caja.encolar_cliente(cliente(nombre, monto))
+        print("Cliente Agregado")
 
     def cobrar_cliente(self):
+        system("cls")
         caja = self.obtener_caja()
         caja.cobrar()
+        print("Se le ha cobrado al cliente")
 
     def mostrar_clientes_encolados_en_caja(self):
+        system("cls")
         caja = self.obtener_caja()
+        print("Total de clientes encolados en caja:")
         print(caja.clientes_encolados)
 
     def mostrar_clientes_atendidos_en_caja(self):
+        system("cls")
         caja = self.obtener_caja()
+        print("Total de clientes atendidos en caja:")
         print(caja.clientes_atendidos)
 
     def mostrar_todos_clientes_encolados(self):
+        system("cls")
+        print("Total de clientes encolados:")
         print(sum(i.clientes_encolados for i in self.__cajas))
 
     def mostrar_todos_clientes_atendidos(self):
+        system("cls")
+        print("Total de clientes atendidos:")
         print(sum(i.clientes_atendidos for i in self.__cajas))
 
     def mostrar_monto_sin_cobrar_en_caja(self):
+        system("cls")
         caja = self.obtener_caja()
+        print("Monto Total sin cobrar en caja:")
         print(caja.monto_sin_cobrar)
 
     def mostrar_monto_cobrado_en_caja(self):
+        system("cls")
         caja = self.obtener_caja()
+        print("Monto Total cobrado en caja:")
         print(caja.monto_obtenido)
 
     def mostrar_monto_total_cobrado(self):
+        system("cls")
+        print("Monto Total cobrado:")
         print(sum(i.monto_obtenido for i in self.__cajas))
 
     def mostrar_monto_total_sin_cobrar(self):
+        system("cls")
+        print("Monto Total sin cobrar:")
         print(sum(i.monto_sin_cobrar for i in self.__cajas))
 
 
