c4b8110b358e932bf51cfa8ce679dfba16db747d
IanBuceta <iansbrizza@gmail.com>
2020-10-23 14:52:03 -0300

    cleaned up code

--- Ejercicios hechos en clase/Ejercicio Dados.py
+++ Ejercicios hechos en clase/Ejercicio Dados.py
@@ -1,6 +1,6 @@
 from random import randint
 from os import system
-from statistics import mean
+# from statistics import mean
 
 system("cls")
 minimo = 1
@@ -12,8 +12,9 @@ resultados = list()
 tiradas = int(input("Ingrese numero de tiradas: "))
 
 for tirada in range(tiradas):
-    resultados.append(randint(minimo,maximo) + randint(minimo, maximo))
+    resultados.append(randint(minimo, maximo) + randint(minimo, maximo))
 
 system("cls")
 for numero in range(2, 13):
-    print("Cantidad de veces que salio {}: {}. Porcentaje total: {}".format(numero, resultados.count(numero), resultados.count(numero) * 100 / len(resultados)))
+    print("Cantidad de veces que salio {}: {}. Porcentaje total: {}".format(
+        numero, resultados.count(numero), resultados.count(numero) * 100 / len(resultados)))
