8e4fbe8c242cd345cd3fe67a213ec787609a20a2
IanBuceta <iansbrizza@gmail.com>
2020-11-03 20:13:11 -0300

    Renamed various files

--- ejercicios/menu/menu.py
+++ ejercicios/menu/menu.py
@@ -1,11 +1,9 @@
-from os import system
-from typing import List
-
 '''
     This class receives a list of tuples (which must have a str, function structure) as a parameter.
     The following is an example code showing a use case for this class,
     in which the user generates a hello_world function and then
-    initializes the class with a list containing a tuple with the desired menu name, and the function it calls    
+    initializes the class with a list containing a tuple with the desired menu name,
+    and the function it calls
 
     def hello_world():
         print("Hello World")
@@ -14,6 +12,8 @@ from typing import List
     screen = Menu([("Hello World", hello_world)])
     screen.show()
 '''
+from os import system
+from typing import List
 
 
 class Menu:
@@ -37,11 +37,3 @@ class Menu:
                 break
             self.__options[option][1]()
             input("Press any key to continue...")
-
-
-def hello_world():
-    print("Hello World")
-
-
-screen = Menu([("Hello World", hello_world)])
-screen.show()
